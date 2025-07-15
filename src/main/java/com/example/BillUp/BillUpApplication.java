a5b2cc1158153f6fe3a3d395ed930c8ef791c049
Hlib T <thiron07@gmail.com>
2025-07-15 09:18:27 +0300

    Added basic structure of the project, no code has been written yet.

---
+++ src/main/java/com/example/BillUp/BillUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.BillUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BillUp/BillUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BillUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
