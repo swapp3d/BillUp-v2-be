9177eca974d09de4891010b66c3e31dcef6a081a
Hlib T <thiron07@gmail.com>
2025-07-15 13:14:55 +0300

    created user model

--- src/main/java/com/example/BillUp/entities/User.java
+++ src/main/java/com/example/BillUp/entities/User.java
@@ -1,24 +1,34 @@
 package com.example.BillUp.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-public class User implements UserDetails {
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    @Override
-    public String getPassword() {
-        return "";
-    }
-
-    @Override
-    public String getUsername() {
-        return "";
-    }
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String surname;
+    //address attribute will be replaced by list of residences (new entity)
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String balance;
+    
 }
