01117eb4226444ba02b94d715e23b981acd44bae
Hlib T <thiron07@gmail.com>
2025-07-16 11:54:44 +0300

    set up for DB

--- src/main/java/com/example/BillUp/entities/User.java
+++ src/main/java/com/example/BillUp/entities/User.java
@@ -7,73 +7,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import com.example.BillUp.enumerators.Role;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
-
-
 @Entity
 @Getter
 @Setter
-@Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "users")
-public class User implements UserDetails {
-
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-  
+    private int id;
     @Enumerated(EnumType.STRING)
     private Role role;
-  
-
-    @Column(nullable = false)
     private String name;
-
-    @Column(nullable = false)
     private String surname;
     //address attribute will be replaced by list of residences (new entity)
-
-    @Column(nullable = false, unique = true)
-    @Email(message = "Invalid email!")
     private String email;
-
-    @Column(nullable = false, unique = true)
     private String phoneNumber;
-
     private String password;
-
-
-    @Column(nullable = false)
-    private String passwordHash;
-
-    private Double balance;
-
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
-    }
-
-    @Override
-    public String getPassword() {
-        return passwordHash;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
 }
