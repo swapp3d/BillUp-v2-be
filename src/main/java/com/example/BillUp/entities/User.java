7552eb9d3074bc847e8430f77176a7f2dd717807
Hlib T <thiron07@gmail.com>
2025-07-16 10:57:43 +0300

    jwt security + user model update

--- src/main/java/com/example/BillUp/entities/User.java
+++ src/main/java/com/example/BillUp/entities/User.java
@@ -1,9 +1,7 @@
 package com.example.BillUp.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.BillUp.enums.Role;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,11 +16,12 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Enumerated(EnumType.STRING)
+    private Role role;
     private String name;
     private String surname;
     //address attribute will be replaced by list of residences (new entity)
     private String email;
     private String phoneNumber;
     private String password;
-    private String balance;
 }
---
+++ src/main/java/com/example/BillUp/security/JwtService.java
@@ -0,0 +1,61 @@
+package com.example.BillUp.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private final String SECRET_KEY;
+    private final long EXPIRATION_MS;
+
+    public JwtService(@Value("${auth.jwt.secret.key}") String secretKey,
+                      @Value("${auth.jwt.expiration-ms}") long expirationMs) {
+        this.SECRET_KEY = secretKey;
+        this.EXPIRATION_MS = expirationMs;
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            return !extractAllClaims(token).getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        return resolver.apply(extractAllClaims(token));
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
